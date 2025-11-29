3e8dfb3eed35ec2a57a7ebb1e57a34c4df3f9360
OxxO98 <hbok1119@naver.com>
2025-11-29 21:37:54 +0900

    api test 2

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false }));
 
-app.use("/test", api_test);
+app.use("/api", api_test);
 
 const port = 5000;
 
