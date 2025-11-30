ef6e49df54926775d48ac150826d72105abf34bc
OxxO98 <hbok1119@naver.com>
2025-11-30 13:28:40 +0900

    patch db_api

--- server/Router/db_api.js
+++ server/Router/db_api.js
@@ -17,7 +17,7 @@ async function getShortURL(connection){
         checkQuery = `
             SELECT USERID, VIDEOID
             FROM URLS
-            WHERE SHORTURL=${shortURL}
+            WHERE SHORTURL='${shortURL}'
         `
 
         ret = await connection.execute(checkQuery);
@@ -39,7 +39,7 @@ async function getUserId(connection) {
         checkQuery = `
             SELECT SHORTURL
             FROM URLS
-            WHERE USERID=${userId}
+            WHERE USERID='${userId}'
         `
 
         ret = await connection.execute(checkQuery);
@@ -77,7 +77,7 @@ async function getLongURL(req, res){
             getQuery = `
                 SELECT DBMS_LOG.GETLENGTH(LONGURL) AS LEN, DBMS_LOB.SUBSTR(LONGURL, 4000, ${offset}) AS STR
                 FROM URLS
-                WHERE SHORTURL=${shortURL}
+                WHERE SHORTURL='${shortURL}'
             `
 
             ret = await connection.execute(getQuery);
