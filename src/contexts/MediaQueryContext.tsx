d8aec5469ad2ce74806195632777c02b4be66d17
OxxO98 <hbok1119@naver.com>
2026-05-15 02:14:37 +0900

    revise @build +6

--- src/contexts/MediaQueryContext.tsx
+++ src/contexts/MediaQueryContext.tsx
@@ -3,5 +3,5 @@ import { createContext } from 'react'
 export const MediaQueryContext = createContext<MediaQueryContextInterface>({
   pc : "(min-width:1024px)",
   tablet : "(min-width:758px) and (max-width:1023px)",
-  mobile : "(max-width:757px)"
+  mobile : "(max-width:757px) or ((orientation: landscape) and (max-height: 520px) and (max-width: 950px) and (hover: none) and (pointer: coarse))"
 })
